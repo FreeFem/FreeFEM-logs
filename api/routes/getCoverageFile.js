d0d4d23f4ae0860a1ecc4a372e145468a83699dd
Simon Garnotel <simon.garnotel@gmail.com>
2019-06-25 15:15:58 +0200

    bug fix

--- api/routes/getCoverageFile.js
+++ api/routes/getCoverageFile.js
@@ -3,13 +3,7 @@ var fs = require('fs')
 var router = express.Router();
 
 /* Get log content */
-router.post('/', function(req, res, next) {
-	/*
-	const directory = req.body.directory
-	const file = req.body.file
-	const content = fs.readFileSync('../../coverage/index.html')
-	res.send(content.text());
-	*/
+router.get('/', function(req, res, next) {
 	const content = fs.readFileSync('../coverage/index.html')
 	res.send(content.text());
 });
