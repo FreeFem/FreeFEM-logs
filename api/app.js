63d257ee16d635eee2170ac859359c1c258f7058
Simon Garnotel <simon.garnotel@gmail.com>
2019-06-25 13:57:32 +0200

    remove some useless code

--- api/app.js
+++ api/app.js
@@ -13,10 +13,6 @@ var getLogContent = require('./routes/getLogContent');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
@@ -43,7 +39,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send({status: 'error'});
 });
 
 module.exports = app;
