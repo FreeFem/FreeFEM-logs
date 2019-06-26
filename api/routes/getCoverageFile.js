d88f7b40087c3c057e64b432d70d9afb43b3c730
Jeremy Croiset <jeremy.croiset@epita.fr>
2019-06-26 11:32:01 +0200

    getCovFile

--- api/routes/getCoverageFile.js
+++ api/routes/getCoverageFile.js
@@ -2,10 +2,12 @@ var express = require('express');
 var fs = require('fs')
 var router = express.Router();
 
-/* Get log content */
-router.get('/', function(req, res, next) {
-	const content = fs.readFileSync('../coverage/index.html')
-	res.send(content.text());
+/* Get coverage html */
+router.post('/', function(req, res, next) {
+	const file = req.body.file
+	console.log('file: '+file)
+	const content = fs.readFileSync(file)
+	res.send(content);
 });
 
 module.exports = router;
